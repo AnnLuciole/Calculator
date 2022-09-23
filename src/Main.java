d8beaa8eb1963b53eee119ace79e323f9ada6dbc
annluciole <annluciole@mail.ru>
2022-09-23 14:43:49 +0500

    Added FileWriter

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,9 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) {
 
-        String scannedString = null;
-
         try (FileReader reader = new FileReader("input.txt")) {
             Scanner sc = new Scanner(reader);
-            scannedString = sc.nextLine().trim();
+            String scannedString = sc.nextLine().trim();
             Checker.checkString(scannedString);
             String[] convertedString = NumbersReader.convert(scannedString);
             System.out.println(Calculator.calculate(convertedString));
