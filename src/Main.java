99d3187fa50a02a82fd4ff6fc60dec6a788cdf5d
annluciole <annluciole@mail.ru>
2022-09-27 15:24:33 +0500

    Время выполнения доведено до необходимого.

--- src/Main.java
+++ src/Main.java
@@ -5,24 +5,28 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        String result = "";
+        ArrayList<String> resultData = new ArrayList<>();
 
         try (FileReader reader = new FileReader("input.txt")) {
             Scanner sc = new Scanner(reader);
             ArrayList<String> dataFromFile = new ArrayList<>();
             while (sc.hasNextByte()) {
                 String scannedString = sc.nextLine();
-                if (scannedString != "") {
+                if (!scannedString.equals("")) {
                     dataFromFile.add(scannedString);
                 }
             }
+            sc.close();
+            String result;
             for (String scannedString : dataFromFile) {
                 try {
                     Checker.checkString(scannedString);
                     NumbersReader.DataForCalculator convertedString = NumbersReader.convert(scannedString);
-                    result = result + scannedString + " = " + Calculator.calculate(convertedString) + "\n" + "\n";
+                    result = scannedString + " = " + Calculator.calculate(convertedString) + "\n" + "\n";
+                    resultData.add(result);
                 } catch (MyException ex) {
-                    result = result + scannedString + " = " + ex.getMessage() + "\n" + "\n";
+                    result = scannedString + " = " + ex.getMessage() + "\n" + "\n";
+                    resultData.add(result);
                 }
             }
         } catch (Exception ex) {
@@ -30,7 +34,9 @@ public class Main {
         }
 
         try (FileWriter writer = new FileWriter("output.txt", true)) {
-            writer.write(result);
+            for (String result : resultData) {
+                writer.write(result);
+            }
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
         }
